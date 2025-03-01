026a49a1fea21929f1fe8c6c5f096524b0b973d0
patha2702 <rajendrapatha2702@gmail.com>
2025-03-01 18:04:48 +0530

    ui: add header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,38 @@
 import React from "react";
+import Image from "next/image";
+import Link from "next/link";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <header className="p-5 bg-black">
+      <div className="container flex justify-between items-center">
+        <Link href="/">
+          <Image
+            src={"/assets/icons/logo.svg"}
+            alt="task-manager"
+            width={40}
+            height={41}
+          />
+        </Link>
+
+        <button className="sm:hidden">
+          <Image
+            src={"/assets/icons/hamburger.svg"}
+            alt="menu"
+            aria-label="open navigation menu"
+            width={20}
+            height={20}
+          />
+        </button>
+        <Link
+          href={"/"}
+          className="py-2.5 px-3.5 bg-white text-black text-[16px] font-bold rounded-[10px] hidden sm:inline-flex justify-center items-center leading-none"
+        >
+          Get for free
+        </Link>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
